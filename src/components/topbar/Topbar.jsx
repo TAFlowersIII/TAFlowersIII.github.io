06b2090c9598e6c69ce38348cc2394210053571f
KhakiSuitValentino <110346660+TAFlowersIII@users.noreply.github.com>
2023-01-02 11:05:09 -0500

    topbar looking great! links formatted

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,13 +5,10 @@ export default function topbar() {
   return (
     <div className="topbar">
       <div className="wrapper">
-        <div className="left">
           <a href='#home' className='homelink'>Home</a>
           <a href='#portfolio' className='portfoliolink'>Portfolio</a>
-        </div>
-        <div className="right"></div>
           <a href='#techstack' className='techstacklink'>Tech Stack</a>
-          <a href='#contact' className='contactlink'>Contact</a>
+          <a href='#about' className='aboutlink'>About</a>
       </div>
     </div>
   )
