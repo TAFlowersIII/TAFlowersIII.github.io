fb45318b7819ce754fc19b8eade6dd3722af1c5a
KhakiSuitValentino <110346660+TAFlowersIII@users.noreply.github.com>
2023-01-12 18:08:08 -0500

    techstack.scss complete

--- src/components/techstack/Techstack.jsx
+++ src/components/techstack/Techstack.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import './Techstack.scss'
-import Topbar from '../topbar/Topbar.jsx'
-import Bottombar from '../bottombar/Bottombar.jsx'
 
 import JavascriptIcon from '/Users/melchior/VSCode_Projects/taflowers-portfolio/src/images/javascript-icon.png'
 import Python3Icon from '/Users/melchior/VSCode_Projects/taflowers-portfolio/src/images/python-icon.png'
@@ -19,64 +17,60 @@ export default function Works() {
 
     <>
 
-    {/* <Topbar /> */}
-
-    <div className='works' id='works'>
+    <div className='stack'>
       <h1>Tech Stack:</h1>
 
       <div className='javascript'>
-        <img src={JavascriptIcon} alt="Javascript" width='55' height='55' />
+        <img src={JavascriptIcon} alt="Javascript" width='60' height='60' />
         <h3>Javascript</h3>
       </div>
 
       <div className='python'>
-        <img src={Python3Icon} alt="Python3" width='55' height='55' />
+        <img src={Python3Icon} alt="Python3" width='60' height='60' />
         <h3>Python3</h3>
       </div>
       
       <div className='sql'>
-        <img src={SQLIcon} alt="SQL also known as Sequel" width='55' height='55' />
+        <img src={SQLIcon} alt="SQL also known as Sequel" width='60' height='60' />
         <h3>PostgreSQL</h3>
       </div>
       
       <div className='mongodb'>
-        <img src={MongoDBIcon} alt="Mongo DB" width='55' height='55' />
+        <img src={MongoDBIcon} alt="Mongo DB" width='60' height='60' />
         <h3>MongoDB</h3>
       </div>
       
       <div className='express'>
-        <img src={ExpressIcon} alt="Express" width='55' height='55' />
+        <img src={ExpressIcon} alt="Express" width='60' height='60' />
         <h3>Express.JS</h3>
       </div>
       
       <div className='nodejs'>
-        <img src={NodeJSIcon} alt="Node JS" width='55' height='55' />
+        <img src={NodeJSIcon} alt="Node JS" width='60' height='60' />
         <h3>Node.JS</h3>
       </div>
       
       <div className='react'>
-        <img src={ReactIcon} alt="React" width='55' height='55' />
+        <img src={ReactIcon} alt="React" width='60' height='60' />
         <h3>React</h3>
       </div>
       
       <div className='django'>
-        <img src={DjangoIcon} alt="Django" width='55' height='55' />
+        <img src={DjangoIcon} alt="Django" width='60' height='60' />
         <h3>Django</h3>
       </div>
       
       <div className='htmlicon'>
-        <img src={HTMLIcon} alt="HTML5" width='55' height='55' />
+        <img src={HTMLIcon} alt="HTML5" width='60' height='60' />
         <h3>HTML</h3>
       </div>
       
       <div className='cssicon'>
-        <img src={CSSIcon} alt="CSS3" width='55' height='55' />
+        <img src={CSSIcon} alt="CSS3" width='60' height='60' />
         <h3>CSS/Tailwind</h3>
       </div>
       
     </div>
-
-    {/* <Bottombar /> */}
 
   </>
 
